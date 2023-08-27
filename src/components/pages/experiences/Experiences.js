925720ce2bf774938379c20ef4167c2a2c05e04d
Sergacova <90244286+Sergacova@users.noreply.github.com>
2023-08-27 16:17:20 +0300

    .

--- src/components/pages/experiences/Experiences.js
+++ src/components/pages/experiences/Experiences.js
@@ -31,7 +31,7 @@ export default function Experiences() {
             </div>
           </div>
           <div className="row mt-top">
-            <div className="col xl6 l6 m6 s12">
+            <div className="col x18 l8 m6 s12">
               <p className="teal year_exp white-text">
                 Jan
                 <strong>2020</strong> - Mar
